f217c39ed1392094c4ed4eb021eddc5ea737fb48
Aymerick Toonba <godzilla.toonba@gmail.com>
2023-06-29 17:20:57 +0200

    fixed how card look on phone

--- src/Components/CardDeck/CardDeck.jsx
+++ src/Components/CardDeck/CardDeck.jsx
@@ -44,8 +44,6 @@ const linkMotion = {
 function CardDeck({ project, isMobile }) {
   const [animationCard1, setAnimationCard1] = useState({ transform: 'rotateY(180deg)' })
   const [animationCard2, setAnimationCard2] = useState({ transform: 'rotateY(0deg)' })
-  const [ref, { height }] = useMeasure()
-  console.log(height)
 
   const handleClickCard1 = () => {
     setAnimationCard1({ transform: 'rotateY(180deg)' })
@@ -61,7 +59,7 @@ function CardDeck({ project, isMobile }) {
       <motion.article initial="rest" whileHover="hover" className="w-full h-full relative">
         {isMobile ? (
           <>
-            <motion.div ref={ref} initial={{ transform: 'rotateY(180deg)' }} animate={animationCard1} transition={{ duration: 1 }} onClick={handleClickCard1} className="w-2/3 [aspect-ratio:6/9] m-auto flex flex-col justify-around absolute top-0 left-0 inset-1 bg-blue-950 rounded-xl overflow-hidden border [transform-style: preserve-3d] [backface-visibility:hidden] border-yellow-200">
+            <motion.div initial={{ transform: 'rotateY(180deg)' }} animate={animationCard1} transition={{ duration: 1 }} onClick={handleClickCard1} className="w-2/3 [aspect-ratio:6/9] m-auto flex flex-col justify-around absolute top-0 left-0 inset-1 bg-blue-950 rounded-xl overflow-hidden border [transform-style: preserve-3d] [backface-visibility:hidden] border-yellow-200">
               <div>
                 <h3 className="w-full text-center text-[4vw] pb-1 underline underline-offset-4 text-yellow-300 font-bagel">Description</h3>
                 <p className="text-[2vw] w-full p-2">{project.description}</p>
@@ -82,10 +80,10 @@ function CardDeck({ project, isMobile }) {
 
             <motion.div initial={{ transform: 'rotateY(0deg)' }} animate={animationCard2} transition={{ duration: 1 }} onClick={handleClickCard2} className="w-2/3 [aspect-ratio:6/9] absolute m-auto [transform-style: preserve-3d] [backface-visibility:hidden] inset-1 border border-yellow-200 bg-blue-950 rounded-xl overflow-hidden">
               <img className="w-full aspect-square" src={project.picture} alt={`${project.title} logo`} />
-              <div className={`flex flex-col gap-4 justify-center w-full items-center h-1/2`}>
-                <h3 className="text-[6vw] w-full text-center md:pt-4 text-yellow-300 font-bagel m-0">{project.title}</h3>
-                <a href={project.link} className="w-1/2 h-[6vw] flex justify-center items-center bg-blue-950 rounded-xl hover:bg-blue-500 border z-20 border-yellow-200">
-                  <div className="w-full md:text-2xl text-[3vw] text-center text-yellow-300 font-bagel">Take a look</div>
+              <div className={`flex flex-col gap-3 justify-center w-full items-center h-1/3`}>
+                <h3 className="text-[6vw] w-full text-center text-yellow-300 font-bagel m-0">{project.title}</h3>
+                <a href={project.link} className="w-1/2 h-[6vw] flex justify-center items-center bg-blue-950 rounded-xl hover:bg-blue-500 border border-yellow-200">
+                  <div className="w-full text-[3vw] text-center text-yellow-300 font-bagel">Take a look</div>
                 </a>
               </div>
             </motion.div>
